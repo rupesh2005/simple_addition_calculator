0f6fd5e40e5ba240c7bdb8f2db45f2c9fe9df015
Rupesh PathaK <rp12062005@gmail.com>
2020-07-31 20:19:22 +0530

    Update First.c
    I have added a 'caution' which says that this program can only add.

--- simple_addition_calculator/First.c
+++ simple_addition_calculator/First.c
@@ -1,12 +1,13 @@
 # include <stdio.h>
 
 int main()
-{   
+{   /*Rupesh Pathak*/
     // This is my first program that I have ever made in C.
     // I am learning C with the help of 'codewithharry' on YouTube.
     // Date: 30/07/2020
 
     int a, b;
+    printf("Caution: This Calculator can only be used for addition.\n");
     printf("Enter Your First Number:");
     scanf("%d", &a);
     printf("Enter Your Second Number:");
