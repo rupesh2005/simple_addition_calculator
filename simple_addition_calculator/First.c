8eb6b034739ba4a3e87ddadcbaed302e56586cae
Rupesh Pathak <63050454+rupesh2005@users.noreply.github.com>
2020-07-31 19:49:57 +0530

    Add files via upload

---
+++ simple_addition_calculator/First.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main()
+{   
+    // This is my first program that I have ever made in C.
+    // I am learning C with the help of 'codewithharry' on YouTube.
+    // Date: 30/07/2020
+
+    int a, b;
+    printf("Enter Your First Number:");
+    scanf("%d", &a);
+    printf("Enter Your Second Number:");
+    scanf("%d", &b);
+    printf("The Sum Of The Both Numbers Is: %d", a+b);
+    return 0;
+}
